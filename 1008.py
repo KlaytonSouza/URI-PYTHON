fab820154b027ea8cfb8f5a5e550bd5c2f63d0f7
Klayton Souza <klayton.almeida@dce.ufpb.br>
2019-11-13 18:42:25 -0300

    1008 Salário

---
+++ 1008.py
@@ -0,0 +1,6 @@
+numeroFuncionario = int(input())
+horasTrabalhos = int(input())
+valorHora = float(input())
+salario = horasTrabalhos*valorHora
+print ("NUMBER = %.i" % numeroFuncionario)
+print ("SALARY = U$ %.2f" % salario)
